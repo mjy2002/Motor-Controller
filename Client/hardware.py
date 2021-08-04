344b320069b22c15b1f95c70247e30e32a0ff81b
AdinAck <adinackerman@gmail.com>
2021-08-03 23:51:22 -0700

    adding more stuff

--- Client/hardware.py
+++ Client/hardware.py
@@ -6,18 +6,23 @@ Adin Ackerman
 """
 
 from threading import Lock
+from itertools import chain
 from serial import Serial
 from serial.serialutil import SerialException
+from dataclasses import dataclass
+from typing import Literal
 
 
 class Motor:
     """
-    A Motor object, representing the communication with a single motor.
+    A Motor object, representing the communication with a single motor
 
     Attributes
     ----------
     ser: Serial
         Serial object
+    port: str
+        Serial port
     id: int
         Motor ID for multi-motor systems
     lock: Lock
@@ -31,9 +36,11 @@ class Motor:
         port: str
             Serial port to connect to
         """
-        self.ser = Serial(port, 9600)
+        self.port = port
+        self.ser = Serial(baudrate=9600)
         self.lock = Lock()
-        self.id = int(self._sendCommand('I'))
+
+        self.connect()
 
     def _sendCommand(self, cmd: str) -> str:
         """
@@ -63,8 +70,9 @@ class Motor:
         """
         Establish connection with motor
         """
-        with self.lock:
-            self.ser.open()
+        self.ser.port = self.port
+        self.ser.open()
+        self.id = int(self._sendCommand('I'))
 
     @property
     def alive(self) -> bool:
@@ -77,6 +85,41 @@ class Motor:
             True if alive, False otherwise
         """
         return self.ser.is_open
+
+    def setPIDs(self, stage: Literal['vel', 'angle'], *args: float, **kwargs: float) -> bool:
+        """
+        Set PID values for angle and velocity control
+
+        Parameters
+        ----------
+        stage: Literal['vel', 'angle']
+            Which PID stage to set
+        P: float
+            Proportional gain
+        I: float
+            Integral gain
+        D: float
+            Differential gain
+        R: float
+            Output ramp
+        L: float
+            Output limit
+        F: float
+            Low pass filter time constant
+
+        Returns
+        -------
+        bool
+            Confirmation
+        """
+        controlType = 'A' if stage == 'angle' else 'V'
+
+        success = True
+        for char, arg in chain(zip(['P', 'I', 'D', 'R', 'L', 'F'], args), kwargs.items()):
+            success &= float(self._sendCommand(
+                f'M{controlType}{char}{arg}')) == arg
+
+        return success
 
     @property
     def position(self) -> float:
@@ -92,7 +135,73 @@ class Motor:
         """
         return float(self._sendCommand('MMG5'))
 
-    def move(self, pos: float) -> float:
+    def setCurrentLimit(self, limit: float) -> bool:
+        """
+        Set motor current limit
+
+        Parameters
+        ----------
+        limit: float
+            Limit value
+
+        Returns
+        -------
+        bool
+            Confirmation
+        """
+        return float(self._sendCommand(f'MLC{limit}')) == limit
+
+    def setVoltageLimit(self, limit: float) -> bool:
+        """
+        Set motor voltage limit
+
+        Parameters
+        ----------
+        limit: float
+            Limit value
+
+        Returns
+        -------
+        bool
+            Confirmation
+        """
+        return float(self._sendCommand(f'MLU{limit}')) == limit
+
+    def setVelocityLimit(self, limit: float) -> bool:
+        """
+        Set motor velocity limit
+
+        Parameters
+        ----------
+        limit: float
+            Limit value
+
+        Returns
+        -------
+        bool
+            Confirmation
+        """
+        return float(self._sendCommand(f'MLV{limit}')) == limit
+
+    def setControlMode(self, mode: Literal['torque', 'velocity', 'angle'] = 'torque') -> bool:
+        """
+        Set control mode
+
+        Parameters
+        ----------
+        mode: str
+            Control mode
+
+            Can be one of 3 literals: 'torque', 'velocity', or 'angle'
+
+        Returns
+        -------
+        bool
+            Confirmation
+        """
+        return self._sendCommand(f'M{mode}')[:3] == mode[:3]
+
+    def move(self, pos: float) -> bool:
         """
         Set target position
 
@@ -103,7 +212,7 @@ class Motor:
 
         Returns
         -------
-        float
-            Confirmed target position
+        bool
+            Confirmation
         """
-        return float(self._sendCommand(f'M{pos}'))
+        return float(self._sendCommand(f'M{pos}')) == pos
--- Client/main.py
+++ Client/main.py
@@ -2,3 +2,4 @@ from hardware import Motor
 
 m = Motor('/dev/cu.usbmodem1243201')
 print(m.id, m.position, m.velocity)
+print(m.setPIDs('angle', 10, D=0.1))
